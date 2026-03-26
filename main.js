c3ca41f744290b57a1945b8308e7c2108c4cac41
maleehais123-debug <maleehais123@gmail.com>
2026-03-26 18:33:01 +0000

    Update main.js

--- main.js
+++ main.js
@@ -61,3 +61,10 @@ burger.addEventListener("click", () => {
     navbar.classList.remove("active");
   });
 });
+
+const burger = document.getElementById("burger");
+const navLinks = document.getElementById("navLinks");
+
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
